488739e0a969bee462d9e1d1a9eec1e5a84db71a
swarajmahadik123 <swarajmahadik1234@gmail.com>
2024-11-22 22:18:45 +0530

    assets updated

---
+++ frontend/src/Assets/images/index.js
@@ -0,0 +1,21 @@
+import aboutBg from "./about-bg.svg";
+import arrow from "./arrow.svg";
+import award from "./award.png";
+import awardsBg from "./awards-bg.png";
+import bgLanding from "./bg-landing.png"
+import bgTexture from "./bg-texture.svg";
+import c1 from "./c1.svg"
+import c2 from "./c2.svg"
+import c3 from "./c3.svg"
+import c4 from "./c4.svg"
+import colabLogo from "./colab-logo.png"
+import demo from "./demo.png"
+import icon1 from "./icon-i.svg"
+import langing from "./landing.svg"
+import linkedin from "./linkedin.png"
+import logoPng from "./logo.png"
+import logoSvg from "./logo.svg"
+import wc1 from "./wc-1.png"
+import wc2 from "./wc-2.png"
+
+export  {aboutBg,arrow,award,awardsBg,bgLanding,bgTexture,c1,c2,c3,c4,colabLogo,demo,icon1,langing,linkedin,logoPng,logoSvg,wc1,wc2};
