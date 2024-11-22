b58161f53f4c9b955a3753e2fc124236a6341e7c
swarajmahadik123 <swarajmahadik1234@gmail.com>
2024-11-23 00:47:21 +0530

    our events ,our supporters,caraosel added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,15 +3,21 @@ import Hero from "../components/Homepage/Herosection";
 import GlimpseOfWork from "../components/Homepage/GlimpseOfWork.jsx"
 import AnimatedCounters from "../components/Homepage/AnimatedCounter.jsx";
 import StoriesSection from "../components/Homepage/StoriesSection.jsx";
+import EventsSection from "../components/Homepage/OurEvents.jsx";
+import SupportersSection from "../components/Homepage/OurSupporter.jsx";
+import ImageCarousel from "../components/Homepage/Caraocel.jsx";
 
 
 const  HomePage =()=>{
     return (
       <>
         <Hero />
+        <ImageCarousel/>
         <AnimatedCounters/>
         <StoriesSection />
         <GlimpseOfWork />
+        <EventsSection/>
+        <SupportersSection/>
       </>
     );
 }
