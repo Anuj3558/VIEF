fe05e78014b3549b5b3b1903e6826732dcc28865
swarajmahadik123 <swarajmahadik1234@gmail.com>
2024-11-29 20:57:31 +0530

    newsletter updated

--- frontend/src/pages/NewsLetter.jsx
+++ frontend/src/pages/NewsLetter.jsx
@@ -58,7 +58,7 @@ const Newsletter = () => {
         <h2 className="text-2xl font-semibold mb-6">Startup Articles</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {newsletters
-            .filter((news) => news.Type === "Article")
+            .filter((news) => news.Type === "article")
             .map((article) => (
               <NewsCard key={article._id} item={article} />
             ))}
